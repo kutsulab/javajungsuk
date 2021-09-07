3aa3d06da2f3537bc68f562204e41d7e19ac98a4
Kutsulab <kutsu920@gmail.com>
2021-09-07 22:19:58 +0900

    자바의 정석 3판 p.8 예제 1-1의 내용 확인 및 연습 : Hello World 출력하기

--- Ch01/src/NoHello.java
+++ Ch01/src/NoHello.java
@@ -1,6 +1,6 @@
 
 public class NoHello {
 	public static void main(String[] args) {
-		System.out.println("NoHello");
+		System.out.println("NoHello"); // println()메서드는 괄호 안의 내용을 출력한다. (줄바꿈 있음)
 	}
 }
