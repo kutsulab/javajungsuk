a490491f18a21ae1a204453ac31b738fb0f39fd7
kutsulab <kutsu920@gmail.com>
2021-09-09 10:34:39 +0900

    Java의 정석 3판 p.457에서 설명한 공변반환타입 기능을 적용하여 clone메서드를 호 출한 곳에서 따로 형변환을 할 필요가 없도록 변경

--- Ch09/src/CloneEx1.java
+++ Ch09/src/CloneEx1.java
@@ -18,7 +18,9 @@ public class CloneEx1 {
 	
 	public static void main(String[] args) {
 		Point original = new Point(3,5); // 원본 Point 인스턴스를 생성
-		Point copy = (Point) original.clone(); // Object 클래스로 형변환 되어서 반환되었으므로 다시 원래 클래스로 형변환해야한다.
+		Point copy = original.clone(); // Object 클래스로 형변환 되어서 반환되었으므로 다시 원래 클래스로 형변환해야한다.
+		// JDK 1.5부터는 공변반환타입 기능이 추가되어, 클래스 내에서 반환 타입을 자손타입(클래스 타입)으로 하여 반환할 수 있다.
+		// 즉 호출부에서 형변환을 따로 할 필요가 없음 
 		System.out.println("original"+original);
 		System.out.println("copy"+copy);
 	}
@@ -41,7 +43,8 @@ class Point  implements Cloneable {
 	}
 	
 	@Override
-	public Object clone() { // 다른 클래스에서 clone()을 호출하려면 public으로 접근제어자를 오버라이드 해야한다.
+	public Point clone() { // 다른 클래스에서 clone()을 호출하려면 public으로 접근제어자를 오버라이드 해야한다.
+		// JDK 1.5부터 공변 반환타입 기능이 추가되어, 오버라이딩을 할 때 조상 메서드의 반환타입을 자손타입으로 변경 허용함
 
 		Object obj = null; // Object obj을 선언 후 null로 초기화
 		
@@ -54,7 +57,8 @@ class Point  implements Cloneable {
 			e.printStackTrace();
 		} 
 		
-		return obj; // Object 클래스의 참조변수로 반환되므로 호출한 쪽에서 원래 클래스로 형변환해야한다.
+		return (Point)obj; // Object 클래스의 참조변수로 반환되므로 호출한 쪽에서 원래 클래스로 형변환해야한다.
+		// 1.5부터 공변 반환타입 기능이 추가되어 class의 clone() 메서드 내에서 자손타입으로 형변환하여 반환 가능. 
 	}
 	
 }
