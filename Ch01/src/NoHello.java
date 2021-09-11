9fd1a74c2be304c33bb1da1df04f1309b2009694
kutsulab <kutsu920@gmail.com>
2021-09-11 17:16:08 +0900

    Java의 정석 p.8 예제 1-1의 내용 확인 및 연습 : println()문 설명 추가

--- Ch01/src/NoHello.java
+++ Ch01/src/NoHello.java
@@ -1,6 +1,16 @@
 
 public class NoHello {
+	
+	/* Java의 정석 p.8 예제 1-1
+	 * 
+	 * System.out.println() 메서드는 ()안의 문자열을 출력하고 줄바꿈을 행한다.
+	 * 
+	 * 기본형 데이터들은 자동으로 문자열로 변환해서 출력한다.
+	 * 객체를 ()안에 매개변수로 넣을 경우 자동으로 toString() 메서드를 호출하여 결과값을 출력한다.
+	 * '+'를 통해 여러 문자열들을 결합해서 새로운 문자열 인스턴스를 생성하고 그 문자열을 화면에 출력해준다.
+	 */
+	
 	public static void main(String[] args) {
-		System.out.println("NoHello"); // println()메서드는 괄호 안의 내용을 출력한다. (줄바꿈 있음)
+		System.out.println("NoHello"); // 문자열 "NoHello"를 출력하는 메서드
 	}
 }
