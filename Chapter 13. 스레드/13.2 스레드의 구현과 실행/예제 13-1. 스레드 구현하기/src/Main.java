35b0923178a150a86edd3d2e63702d75014c0136
ttasjwi <ttasjwi920@gmail.com>
2022-01-22 17:02:46 +0900

    [Thread] 스레드 구현하기
    - 람다식으로 Runnable 구현하여 Thread 구현하는 방법 추가

--- Chapter 13. 스레드/13.2 스레드의 구현과 실행/예제 13-1. 스레드 구현하기/src/Main.java
+++ Chapter 13. 스레드/13.2 스레드의 구현과 실행/예제 13-1. 스레드 구현하기/src/Main.java
@@ -7,18 +7,26 @@ public class Main {
 
         Thread thread1 = new ThreadEx1(); // Thread의 하위 클래스에서 run메서드 구현 후, 사용
         Thread thread2 = new Thread(new ThreadEx2()); // Runnable 구현체를 인자로 상속하기
+        Thread thread3 = new Thread(() -> {
+            for (int i=0; i<100; i++) {
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }); // 람다식으로 Runnable 구현하고, 이를 통해 Thread 생성
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
         String thisThreadName = Thread.currentThread().getName();
-
-
         for (int i=0; i<100; i++) {
             System.out.println(thisThreadName);
             Thread.sleep(100); // 스레드를 100ms 멈춘다.
         }
-
     }
 
 }
